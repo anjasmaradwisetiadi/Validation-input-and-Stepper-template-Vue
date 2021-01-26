869ed440985d49b7da79503fe66f43d235b70e49
anjasmaradwisetiadi <enzoblackred@gmail.com>
2021-01-27 01:00:21 +0700

    succesfull use number and alphabet

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,76 +28,39 @@
     directives: {
       digitsonly(el, binding) {
         el.addEventListener('keypress', (e) => {
-          if ([46, 8, 9, 27, 13, 110, 190].indexOf(e.keyCode) !== -1 ||
-            // Allow: Ctrl+A
-            (e.keyCode === 65 && e.ctrlKey === true) ||
-            // Allow: Ctrl+C
-            (e.keyCode === 67 && e.ctrlKey === true) ||
-            // Allow: Ctrl+X
-            (e.keyCode === 88 && e.ctrlKey === true) ||
-            // Allow: home, end, left, right
-            (e.keyCode >= 35 && e.keyCode <= 39)) {
-            // let it happen, don't do anything
-            return
+          const key = e.key;
+          if(!/\d/.test(key)){
+            console.log("only number")
+             e.preventDefault();
           }
-          // Ensure that it is a number and stop the keypress
-          if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e
-              .keyCode > 105)) {
-            e.preventDefault()
-            console.log("string")
-          }
-          // const key = e.key;
-          // if(!/\d/.test(key)){
-          //   console.log("only number")
-          //    e.preventDefault();
-          // }
         })
 
         el.addEventListener('change', (e) => {
           const initialValue = el.value;
           console.log(initialValue)
           el.value = initialValue.replace(/[^0-9]*/g, '');
-          console.log("1")
-          console.log(el.value)
-          console.log("2")
-          console.log(initialValue)
           if (initialValue !== el.value) {
             e.stopPropagation()
-            // console.log("string")
           }
-          // e.target.dispatchEvent(e);
         })
 
       },
 
       Alphabet(el, binding) {
         el.addEventListener('keypress', (e) => {
-            if ([46, 8, 9, 27, 13, 110, 190].indexOf(e.keyCode) !== -1 ||
-              // Allow: Ctrl+A
-              (e.keyCode === 65 && e.ctrlKey === true) ||
-              // Allow: Ctrl+C
-              (e.keyCode === 67 && e.ctrlKey === true) ||
-              // Allow: Ctrl+X
-              (e.keyCode === 88 && e.ctrlKey === true) ||
-              // Allow: home, end, left, right
-              (e.keyCode >= 35 && e.keyCode <= 39)) {
-              // let it happen, don't do anything
-              return
+            const key = e.key;
+            if(!/[A-Za-z]/.test(key)){
+              console.log("only string")
+              e.preventDefault();
             }
           }),
           el.addEventListener('change', (e) => {
             const initialValue = el.value;
-            console.log(initialValue)
             el.value = initialValue.replace(/[^A-Za-z ]*/g, '');
-            console.log("1")
-            console.log(el.value)
-            console.log("2")
-            console.log(initialValue)
             if (initialValue !== el.value) {
               e.stopPropagation()
-              // console.log("string")
+              console.log("string")
             }
-            // e.target.dispatchEvent(e);
           })
       },
     }
