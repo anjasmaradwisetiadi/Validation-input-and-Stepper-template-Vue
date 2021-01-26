6f52c2cc09b927015a94f66759b18aa4ac0e39a4
anjasmaradwisetiadi <enzoblackred@gmail.com>
2021-01-27 00:22:35 +0700

    can directive for number and alphabet

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,61 +1,128 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="form-group">
+      <label for="number">Number Only </label>
+      <input v-model="myNumber" v-digitsonly type="text" name="number" class="mx-3">
+    </div>
+
+    <div class="form-group pt-3">
+      <label for="text">Text Only </label>
+      <input v-model="myText" v-Alphabet type="text" name="text" class="mx-3">
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
-};
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        myNumber: null,
+        myText:'',
+      }
+    },
+    props: {
+      msg: String,
+    },
+    directives: {
+      digitsonly(el, binding) {
+        el.addEventListener('keypress', (e) => {
+          if ([46, 8, 9, 27, 13, 110, 190].indexOf(e.keyCode) !== -1 ||
+            // Allow: Ctrl+A
+            (e.keyCode === 65 && e.ctrlKey === true) ||
+            // Allow: Ctrl+C
+            (e.keyCode === 67 && e.ctrlKey === true) ||
+            // Allow: Ctrl+X
+            (e.keyCode === 88 && e.ctrlKey === true) ||
+            // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+            // let it happen, don't do anything
+            return
+          }
+          // Ensure that it is a number and stop the keypress
+          if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e
+              .keyCode > 105)) {
+            e.preventDefault()
+            console.log("string")
+          }
+          // const key = e.key;
+          // if(!/\d/.test(key)){
+          //   console.log("only number")
+          //    e.preventDefault();
+          // }
+        })
+
+        el.addEventListener('change', (e) => {
+          const initialValue = el.value;
+          console.log(initialValue)
+          el.value = initialValue.replace(/[^0-9]*/g, '');
+          console.log("1")
+          console.log(el.value)
+          console.log("2")
+          console.log(initialValue)
+          if (initialValue !== el.value) {
+            e.stopPropagation()
+            // console.log("string")
+          }
+          // e.target.dispatchEvent(e);
+        })
+
+      },
+
+      Alphabet(el, binding) {
+        el.addEventListener('keypress', (e) => {
+            if ([46, 8, 9, 27, 13, 110, 190].indexOf(e.keyCode) !== -1 ||
+              // Allow: Ctrl+A
+              (e.keyCode === 65 && e.ctrlKey === true) ||
+              // Allow: Ctrl+C
+              (e.keyCode === 67 && e.ctrlKey === true) ||
+              // Allow: Ctrl+X
+              (e.keyCode === 88 && e.ctrlKey === true) ||
+              // Allow: home, end, left, right
+              (e.keyCode >= 35 && e.keyCode <= 39)) {
+              // let it happen, don't do anything
+              return
+            }
+          }),
+          el.addEventListener('change', (e) => {
+            const initialValue = el.value;
+            console.log(initialValue)
+            el.value = initialValue.replace(/[^A-Za-z ]*/g, '');
+            console.log("1")
+            console.log(el.value)
+            console.log("2")
+            console.log(initialValue)
+            if (initialValue !== el.value) {
+              e.stopPropagation()
+              // console.log("string")
+            }
+            // e.target.dispatchEvent(e);
+          })
+      },
+    }
+  };
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue';
-import App from './App.vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import router from './router';
 import store from './store';
+import App from './App.vue';
+import { JustDigits } from './Directive/filter';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+JustDigits();
 
 Vue.config.productionTip = false;
 
