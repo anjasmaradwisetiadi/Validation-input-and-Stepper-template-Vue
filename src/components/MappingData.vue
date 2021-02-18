2acfd689f240a0fd660b4c28b6606e6fcac60c24
anjasmaradwisetiadi <enzoblackred@gmail.com>
2021-02-18 21:08:42 +0700

    successfull delete data

---
+++ src/components/MappingData.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="mapping_data">
+    <hr>
+    <button class="btn btn-warning" @click="reduce()"> Reduce Data </button>
+    <div class="row">
+      <div class="col">
+        <span> <strong>Before : </strong> </span>
+        <pre class="text-left" id="json">
+        {{dataOriSiswa}}
+        </pre>
+        <br>
+      </div>
+      <div class="col">
+        <span> <strong>After :</strong> </span>
+
+        <pre class="text-left">
+            {{dataAfter}}
+        </pre>
+      </div>
+    </div>
+
+  </section>
+</template>
+<script>
+  import {
+    dataSiswa
+  } from '../Data_Resource/dataMurid'
+
+  export default {
+    data() {
+      return {
+        dataBefore: '',
+        containArray:[],
+        dataAfter: '',
+      }
+    },
+
+    computed: {
+      dataOriSiswa() {
+        return this.dataBefore = JSON.stringify(dataSiswa, undefined, 2);
+      }
+    },
+    methods: {
+      reduce() {
+        dataSiswa.forEach((Element) => {
+          delete Element.no_telp;
+          delete Element.tanggal_lahir;
+          this.containArray.push(Element)
+          this.dataAfter=JSON.stringify(this.containArray,undefined,2);
+        })
+      }
+    }
+
+  }
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App "/>
-    <image-priview></image-priview>
+    <div class="row">
+      <div class="col">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App " />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <image-priview></image-priview>
+      </div>
+      <div class="col">
+        <mapping-data></mapping-data>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-import ImagePriview from '../components/ImagePriview.vue';
+  // @ is an alias to /src
+  import HelloWorld from '@/components/HelloWorld.vue';
+  import ImagePriview from '../components/ImagePriview.vue';
+import MappingData from '../components/MappingData.vue';
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-    ImagePriview,
-  },
-};
+  export default {
+    name: 'Home',
+    components: {
+      HelloWorld,
+      ImagePriview,
+        MappingData,
+    },
+  };
+
 </script>
